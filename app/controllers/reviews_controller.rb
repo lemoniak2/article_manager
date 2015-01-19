3a1bde62de21e06cd19c2ac4ecfbf5af3b594e55
lemoniak2 <l3moniak2@gmail.com>
2015-01-19 17:44:59 +0100

    Update review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,41 +1,21 @@
 class ReviewsController < ApplicationController
   before_filter :authenticate_user!
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
-
-  respond_to :html
-
-  def index
-    @reviews = Review.all
-    respond_with(@reviews)
-  end
-
-  def show
-    respond_with(@review)
-  end
-
-  def new
-    @review = Review.new
-    respond_with(@review)
-  end
-
-  def edit
-  end
+  expose(:review) { review_params }
+  expose(:reviews)
 
   def create
-    @review = Review.new(review_params)
-    @review.article_id = params[:article_id]
-    flash[:notice] = 'Review was successfully created.' if @review.save
+    review = Review.new(review_params)
+    review.article_id = params[:article_id]
+    flash[:notice] = 'Review was successfully created.' if review.save
     redirect_to articles_path
   end
 
   def update
-    flash[:notice] = 'Review was successfully updated.' if @review.update(review_params)
-    respond_with(@review)
+    flash[:notice] = 'Review was successfully updated.' if review.update(review_params)
   end
 
   def destroy
-    @review.destroy
-    respond_with(@review)
+    review.destroy
   end
 
   private
